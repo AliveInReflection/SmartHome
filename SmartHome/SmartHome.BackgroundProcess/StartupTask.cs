3f7d343f973165578ab7d992d50f9539eb351c26
Yurii <Yurii_Honchar@epam.com>
2017-08-16 12:37:14 +0300

    Init commit

---
+++ SmartHome/SmartHome.BackgroundProcess/StartupTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace SmartHome.BackgroundProcess
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            while (true)
+            {
+                System.Diagnostics.Debug.WriteLine("Tick");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
