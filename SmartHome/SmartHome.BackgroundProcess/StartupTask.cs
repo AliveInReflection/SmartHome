7c1a62ea8c08e276a480505a1bd7b604126afe27
Yurii <Yurii_Honchar@epam.com>
2017-08-25 12:27:03 +0300

    Debug works

--- SmartHome/SmartHome.BackgroundProcess/StartupTask.cs
+++ SmartHome/SmartHome.BackgroundProcess/StartupTask.cs
@@ -12,12 +12,16 @@ namespace SmartHome.BackgroundProcess
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        private BackgroundTaskDeferral _defferal;
+
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
+            _defferal = taskInstance.GetDeferral();
+
             while (true)
             {
                 System.Diagnostics.Debug.WriteLine("Tick");
-                await Task.Delay(1000);
+                await Task.Delay(10);
             }
         }
     }
